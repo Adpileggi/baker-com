2408a5d4e3e3d29c21a779745bf5cefa15dd6b16
Anthony Pileggi <adpileggi@gmail.com>
2024-08-05 14:54:32 -0400

    created models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Product = require("./Products");
+const User = require("./User");
+const Category = require("./category");
+const Order = require("./order");
+
+module.exports = { User, Product, Category, Order}
